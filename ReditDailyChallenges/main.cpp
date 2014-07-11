7f24c48317c8011c38bb8a05a0d38bdf5032e987
Aamir Jawaid <lordwizards@gmail.com>
2014-07-10 20:32:08 -0400

    Initial Commit

---
+++ ReditDailyChallenges/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ReditDailyChallenges
+//
+//  Created by Aamir Jawaid on 2014-07-10.
+//  Copyright (c) 2014 Aamir Jawaid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
